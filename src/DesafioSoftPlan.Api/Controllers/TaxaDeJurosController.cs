f4f91433cb26f7f49fd1514891c2cf3ab60332ab
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-05-03 09:15:44 -0300

    Cria endpoint que retorna a taxa de juros

---
+++ src/DesafioSoftPlan.Api/Controllers/TaxaDeJurosController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioSoftPlan.Api.Controllers
+{
+    [Route("taxaJuros")]
+    [ApiController]
+    public class TaxaDeJurosController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(0.01);
+        }
+    }
+}
