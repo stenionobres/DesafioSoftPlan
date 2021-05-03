f9ed01633c64c798fb7a717bdd7f2c03935461b9
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-05-03 10:16:40 -0300

    Adiciona endpoint que retorna a url do código fonte no GitHub

---
+++ src/DesafioSoftPlan.Api/Controllers/CalculaJurosController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DesafioSoftPlan.Api.Controllers
+{
+    
+    [ApiController]
+    public class CalculaJurosController : ControllerBase
+    {
+        [HttpGet]
+        [Route("showmethecode")]
+        public ActionResult Code()
+        {
+            return Ok("https://github.com/stenionobres/DesafioSoftPlan");
+        }
+    }
+}
