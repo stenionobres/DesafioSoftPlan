31db329e02ad2dc7c1d8e90cda528600070a2ace
Stenio Nobres Batista <stenio.nobres@gmail.com>
2021-05-03 07:30:19 -0300

    Configura projeto para testes unitários

---
+++ src/DesafioSoftPlan.TestesUnitarios/Negocio/JurosCompostosTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace DesafioSoftPlan.TestesUnitarios.Negocio
+{
+    [TestFixture]
+    public class JurosCompostosTest
+    {
+        [Test]
+        public void DeveCalcularJurosCompostosIgualAZeroParaValorInicialIgualAZero()
+        {
+
+            Assert.AreEqual(0, 0);
+        }
+    }
+}
